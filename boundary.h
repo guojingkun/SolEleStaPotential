4383188a074eb8bfed9c4498663bf25ec8556451
guojingkun <guojingkun@pku.edu.cn>
2014-10-09 15:44:31 +0800

    Create boundary.h

---
+++ boundary.h
@@ -0,0 +1,15 @@
+/*
+ * guojk leizj
+ */
+
+#ifndef BOUNDARY_H_
+#define BOUNDARY_H_
+
+#include "mypara.h"
+#include "boundary.cpp"
+#include "Point.h"
+
+void BoundaryCondition(CPoint*** Point);
+void CheckBoundaryCondition(CPoint***);
+
+#endif
